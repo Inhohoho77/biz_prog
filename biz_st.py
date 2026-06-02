7bbbcfc5851fdaa5050dc0c79fd91767578dd582
Inhohoho77 <aktnflfjs@gmail.com>
2026-06-02 14:41:47 +0900

    Update biz_st.py

--- biz_st.py
+++ biz_st.py
@@ -1,4 +1,29 @@
 import streamlit as st
 
 st.title("Hello Streamlit")
-st.write("성공!")
+
+st.header("헤더입니다")
+st.subheader("서브헤더입니다")
+
+st.write("일반 텍스트")
+
+st.markdown("# Markdown H1")
+st.markdown("## Markdown H2")
+st.markdown("### Markdown H3")
+
+st.write("😀 Streamlit 공부중")
+
+"""
+### 텍스트 색상 변경
+
+:red[맨유]
+
+:blue[첼시]
+
+:green[맨시티]
+
+:orange[아스날]
+
+:gray[토트넘]
+"""
+
